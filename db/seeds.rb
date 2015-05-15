67aaf365ba923154952106d9c3ce805bd5cd2aaa
Jon Engelbert <jon.a.engelbert@gmail.com>
2015-05-15 08:08:07 -0400

    new db seeds for admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+User.create!(name:  "Admin User",
+             email: "admin@admin.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -14,15 +14,14 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password,
+#                activated: true,
+#                activated_at: Time.zone.now)
+# end
